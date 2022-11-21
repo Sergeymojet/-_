b492fc8367deebd8aa48ab49c769a48466989151
Sergeymojet <sergeymojet@yandex.ru>
2022-11-21 22:21:36 +0300

    Task4

---
+++ 1lesson/4.0/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+int N = int.Parse(a);
+int NegN = - N;
+
+while(NegN <= N)
+{
+    Console.Write($"{NegN} ");
+    NegN +=1;
+}
