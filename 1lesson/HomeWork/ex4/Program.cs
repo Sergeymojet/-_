4f762e161cfd47b4b351298bf2e7908f03230833
Sergeymojet <sergeymojet@yandex.ru>
2022-11-23 21:29:06 +0300

    EX4

---
+++ 1lesson/HomeWork/ex4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+for (int a = 1; a <= num; a++)
+    if (a % 2 == 0)
+         Console.WriteLine(a);
