cd451a3e40f2f9da826710a11228c761182b621f
Sergeymojet <sergeymojet@yandex.ru>
2022-11-28 20:35:44 +0300

    exp3_0

---
+++ C##/2lesson/HomeWork2/ex2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(number);
+}
