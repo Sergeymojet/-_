76e8041ac722895a4af3421080896dbaea70c3ce
Sergeymojet <sergeymojet@yandex.ru>
2022-11-30 20:22:23 +0300

    hw2_2

---
+++ C##/2lesson/HomeWork2/HW2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 2:
+//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+string Number(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+        return $"{num} {num / 100 * 10 + num % 10}";
+    return "Число не 3-х значное";
+}
+
+Console.WriteLine(Number(int.Parse(Console.ReadLine())));
