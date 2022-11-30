7094fbdb8d256c9eebcf6c4dd2f224d8143ce1a8
Sergeymojet <sergeymojet@yandex.ru>
2022-11-30 20:43:46 +0300

    hw2_4

---
+++ C##/2lesson/HomeWork2/HW2_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 4:
+//Напишите программу, которая принимает на вход цифру,
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+void Week(int num)
+{
+    string text = "not_weekend";
+
+    if (num == 7 || num == 6) text = "weekand!";
+    Console.WriteLine($"{num} {text}");
+}
+
+Week(int.Parse(Console.ReadLine()));
