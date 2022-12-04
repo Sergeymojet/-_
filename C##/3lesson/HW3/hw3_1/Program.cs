53be45045be164595778ceeb7578634b253a3331
Sergeymojet <sergeymojet@yandex.ru>
2022-12-04 11:49:25 +0300

    hw3_1

---
+++ C##/3lesson/HW3/hw3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//
+
+
+string Palindrome(int n)
+{  
+    int num = 10000;
+    if (n > 100000 || n < 10000)
+        return "неправильное число";
+    else        
+        while (num > 1)
+        {
+            if (n / num % 10 != n % 10)
+                return "Not palindrome";
+            n /= 10;
+            num /= 100;
+        }    
+        return "Palindrome";
+}
+
+Console.WriteLine(Palindrome(int.Parse(Console.ReadLine())));
+
+
