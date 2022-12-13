dacd04049a052643acc009f97f840f52ea6ade35
Sergeymojet <sergeymojet@yandex.ru>
2022-12-13 22:18:16 +0300

    hw1

--- C##/6lesson/HW6/hw1/Program.cs
+++ C##/6lesson/HW6/hw1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int CountNum()
+{
+    int count = 0;
+    string word;
+
+    while (true)
+    {
+        Console.Write("enter number: ");
+        word = Console.ReadLine();
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count += 1;        
+    }
+}
+
+Console.Write(CountNum());
